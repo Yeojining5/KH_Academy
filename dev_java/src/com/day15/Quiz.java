d592387ce1b880c8ef72c46833ff9a7d45c72c08
BBiroo <yeojining5@gmail.com>
2022-05-10 16:46:50 +0900

    Array Quiz (My version)

--- dev_java/src/com/day15/Quiz.java
+++ dev_java/src/com/day15/Quiz.java
@@ -5,20 +5,31 @@ public class Quiz {
 // 총점 평균 석차를 구하는 프로그램 작성
 	public static void main(String[] args) {
 		int jumsu[] = new int[5];
-
-		jumsu[0] = (int) (Math.random() * 100) + 1;
-		jumsu[1] = (int) (Math.random() * 100) + 1;
-		jumsu[2] = (int) (Math.random() * 100) + 1;
-		jumsu[3] = (int) (Math.random() * 100) + 1;
-		jumsu[4] = (int) (Math.random() * 100) + 1;
-
-		System.out.println(jumsu[0] + ", " + jumsu[1] + ", " + jumsu[2] + ", " + jumsu[3] + ", " + jumsu[4]);
-
+		int rank[] = new int[5];
 		int sum = 0;
-
+		
 		for (int i = 0; i < jumsu.length; i++) {
-			sum += jumsu[i];
+			jumsu[i] = (int) (Math.random() * 100) + 1;
+			sum += jumsu[i];			
 		}
+		
+		for(int i=0;i<jumsu.length;i++) {
+			rank[i] = 1;
+		}
+		
+		for(int i=0; i<jumsu.length;i++) {
+			rank[i] = 1;
+			for(int j=0;j<jumsu.length;j++) {
+				if(jumsu[i] < jumsu[j]) {
+					rank[i] = rank[i]+1; // j점수보다 작으면 순위 증가
+				}
+			}
+		}
+		
+		for (int i = 0; i < jumsu.length; i++) {
+			System.out.println("점수"+ (i+1)+" : "+jumsu[i]+ " (석차 : "+rank[i]+")");
+		}
+		
 		System.out.println("총점 : " + sum);
 		double avg = sum / jumsu.length;
 		System.out.println("평균 : " + avg);
---
+++ dev_java/src/com/day15/Quiz2.java
@@ -0,0 +1,40 @@
+package com.day15;
+
+import java.util.Random;
+
+public class Quiz2 {
+	// 자바 성적을 난수를 발생시켜 5개 받은후
+	// 총점 평균 석차를 구하는 프로그램 작성
+	public static void main(String[] args) {
+		Random r = new Random();
+		int jumsu[] = new int[5];
+		int rank[] = new int[5];
+		int sum = 0;
+		
+		for(int i=0;i<jumsu.length;i++) {
+			jumsu[i] = r.nextInt(101);
+			sum += jumsu[i];		
+		}
+		
+		for(int i=0;i<jumsu.length;i++) {
+			rank[i] = 1;
+		}
+		
+		for(int i=0; i<jumsu.length;i++) {
+			rank[i] = 1;
+			for(int j=0;j<jumsu.length;j++) {
+				if(jumsu[i] < jumsu[j]) {
+					rank[i] = rank[i]+1; // j점수보다 작으면 순위 증가
+				}
+			}
+		}
+		for(int i=0;i<jumsu.length;i++) {
+		System.out.println("점수"+ (i+1)+" : "+jumsu[i]+ " (석차 : "+rank[i]+")");
+	}
+		System.out.println("총점 : "+sum);
+		double avg = sum / jumsu.length;
+		System.out.println("평균 : "+avg);
+		
+	}
+
+}
