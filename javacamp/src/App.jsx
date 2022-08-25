40f70f49bebadf5a77d53f2b2d358e24c9634848
yeojining5 <yeojining5@gmail.com>
2022-08-26 00:04:49 +0900

    220825 Yarn - javacamp

---
+++ javacamp/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import "bootstrap/dist/css/bottstrap.min.css"
+import { Route, Routes } from 'react-router-dom';
+import Login from "./components/pages/Login"
+import Main from "./components/pages/Main"
+import HackerNews from "./components/pages/HackerNews"
+
+function App({ authService }) {
+  return (
+    <>
+      <Routes>
+        <Route path="/" exact={true} element={<Main />} />
+        <Route path="/main/:userId" 
+              exact={true} 
+              element={<Main authService={authService} />} />
+        <Route path="/hacker" exact={true} element={<HackerNews />} />
+      </Routes>
+    </>  
+  );
+}
+
+export default App;
---
+++ javacamp/src/components/pages/HackerNews.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Top from './../header/Top';
+import Bottom from './../footer/Bottom';
+
+function HackerNews(props) {
+  return (
+    <>
+      <Top />
+        <div>해커뉴스</div>
+      <Bottom />
+    </>
+  );
+}
+
+export default HackerNews;
